bdc794491fd4630ea3f2a19b083f9f02003528f7
Andrei Croitoru <andrei.croitoru@macys.com>
2018-10-01 17:44:56 +0300

    Quick sort Add higlight.js Add loading of scripts with fetch and TextDecoder

---
+++ js/src/quick-sort.js
@@ -0,0 +1,43 @@
+
+// Select a pivot - in this case the last element
+// Partition the array into 2, 
+// left from partitionIndex will be values smaller than pivot
+// right from partitionIndex will be values higher than pivot
+// do the same for the two smaller arrays
+
+const quickSort = (arr, left, right) => {
+  
+  if(left < right) {
+    let pivot = right;
+    let partitionIndex = partition(arr, pivot, left, right);
+  
+    quickSort(arr, left, partitionIndex-1);
+    quickSort(arr, partitionIndex+1, right);
+  }
+  
+  return arr;
+}
+
+
+const partition = (arr, pivot, left, right) => {
+
+  let partitionIndex = left;
+    
+  for(let i=left; i<right; i++) {
+    if(arr[i] < arr[pivot]) {
+      swap(arr, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  
+  swap(arr, partitionIndex, pivot);
+  return partitionIndex;
+}
+
+const swap = (arr, i, j) => {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+console.log(quickSort([4, 6, 3, 2, 9, 5], 0, 5));
+console.log(quickSort([4, 6, 3, 2, 2, 5], 0, 5));
+
